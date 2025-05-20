8e3cccf0f2ef6ebb68f4201759c0368a8f907b28
DiVaSkibo <d.v.skibo@student.khai.edu>
2025-05-20 13:29:46 +0200

    Fix
    * setting script

--- script/___setting.gd
+++ script/___setting.gd
@@ -1,30 +1,44 @@
 extends VBoxContainer
 
+#			Vars
+@onready var button_accept :Button = $ButtonAccept
 @onready var menu_create :MenuButton = $MenuCreate
 @onready var menu_graphic :MenuButton = $MenuGraphic
 @onready var menu_audio :MenuButton = $MenuAudio
 
 
+#			Funcs
 func _ready() -> void:
+	hide()
 	menu_create.get_popup().id_pressed.connect(_on_menu_create_pressed)
 	menu_graphic.get_popup().id_pressed.connect(_on_menu_graphic_pressed)
 	menu_audio.get_popup().id_pressed.connect(_on_menu_audio_pressed)
 func _input(event :InputEvent) -> void:
-	if event.is_action_pressed("setting"):
-		if visible: hide()
+	if event.is_action_pressed("selection") or event.is_action_released("setting"):
+		if visible: display(false)
 		else:
 			global_position = get_global_mouse_position()
-			show()
-	elif event is not InputEventMouseMotion and not event.is_action_released("setting") and event.is_action_pressed("selection") and event.is_action_released("selection"):
-		print(event)
-		if visible: hide()
+			if not visible: display()
+	elif event is not InputEventMouseMotion and event.is_action_pressed("selection") and event.is_action_released("selection"):
+		if visible: display(false)
+
+func display(is_displaying :bool = true) -> void:
+	visible = is_displaying
+	button_accept.visible = is_displaying
+	menu_create.visible = is_displaying
+	menu_graphic.visible = is_displaying
+	menu_audio.visible = is_displaying
 
 
+#			Signals
+func _on_button_accept_button_up() -> void:
+	print("||")
+	display(false)
 func _on_menu_create_pressed(id: int) -> void:
 	match id:
 		0: FlowHandler.create_doc("Report")
 		1: FlowHandler.create_doc("Article")
-	hide()
+	display(false)
 func _on_menu_graphic_pressed(id: int) -> void:
 	if not menu_graphic.get_popup().is_item_checked(id):
 		var value
